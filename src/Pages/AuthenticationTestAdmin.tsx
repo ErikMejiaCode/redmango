4f449e75c667c631320a21d3746ad4fbc4727fd9
Erik Mejia <erikdojo352@gmail.com>
2023-06-08 19:18:15 -0400

    Admin authorization using HOC

--- src/Pages/AuthenticationTestAdmin.tsx
+++ src/Pages/AuthenticationTestAdmin.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { withAdminAuth } from "../HOC";
 
 function AuthenticationTestAdmin() {
   return <div>This page can only be accessed by ADMIN level users.</div>;
 }
 
-export default AuthenticationTestAdmin;
+export default withAdminAuth(AuthenticationTestAdmin);
