15db0789f4917f23cfb509942c508e3c09920d49
Erik Mejia <erikdojo352@gmail.com>
2023-06-08 10:15:30 -0400

    token added to local storage. Decoded JWT token

--- src/Components/Page/Login.tsx
+++ src/Components/Page/Login.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import { inputHelper } from "../../Helper";
+import { useLoginUserMutation } from "../../apis/authApi";
+import { apiResponse, userInterface } from "../../Interfaces";
+import jwt_Decode from "jwt-decode";
+import { useDispatch } from "react-redux";
+import { setLoggedInUser } from "../../Storage/Redux/UserAuthSlice";
 
 function Login() {
+  const [error, setError] = useState("");
+  const [loginUser] = useLoginUserMutation();
   const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
   const [userInput, setUserInput] = useState({
     userName: "",
     password: "",
@@ -13,9 +21,33 @@ function Login() {
     setUserInput(tempData);
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const response: apiResponse = await loginUser({
+      userName: userInput.userName,
+      password: userInput.password,
+    });
+
+    if (response.data) {
+      console.log(response.data);
+      const { token } = response.data.result;
+      const { fullName, id, email, role }: userInterface = jwt_Decode(token);
+      localStorage.setItem("token", token);
+
+      dispatch(setLoggedInUser({ fullName, id, email, role }));
+    } else if (response.error) {
+      console.log(response.error.data.errorMessages[0]);
+      setError(response.error.data.errorMessages[0]);
+    }
+
+    setLoading(false);
+  };
+
   return (
     <div className="container text-center">
-      <form method="post">
+      <form method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Login</h1>
         <div className="mt-5">
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -44,6 +76,7 @@ function Login() {
         </div>
 
         <div className="mt-2">
+          {error && <p className="text-danger">{error}</p>}
           <button
             type="submit"
             className="btn btn-success"
