60f15f684026f56ed82c461e9ca4456ed7756168
Erik Mejia <erikdojo352@gmail.com>
2023-06-09 12:11:42 -0400

    stripe integration

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { PaymentForm } from "../Components/Page/Payment";
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
 
-  console.log(apiResult);
-  console.log(userInput);
+  const stripePromise = loadStripe(
+    "pk_test_51N3nZUDsUPrv0h1VPuy3eVrV27n5hX4cXdjH4n5FODaaCl9wUnSsBgw4P4LbWniN5RunB0kgmxEuctB6rR6DiTlC00382jH1Gk"
+  );
+  const options = {
+    //passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
 
-  return <div>Payment</div>;
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
