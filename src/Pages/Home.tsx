7a8adfb3eb8abf161a4e854ddffc495eb68ef27a
Erik Mejia <erikdojo352@gmail.com>
2023-06-16 13:01:29 -0400

    search banner added to home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Banner } from "../Components/Page/Common";
 import { MenuItemList } from "../Components/Page/MenuItems";
 
 function Home() {
   return (
     <div>
+      <Banner />
       <div className="container p-2">
         <MenuItemList />
       </div>
